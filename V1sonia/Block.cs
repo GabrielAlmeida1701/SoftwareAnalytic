f4fe4c02a3d4bed53977e818cf6ba02a6552984e
Amilson  Junior <amilson_junior@outlook.com>
2016-10-11 02:01:46 -0300

    class block initialized

---
+++ V1sonia/Block.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V1sonia
+{
+    class Block
+    {
+        /*
+         Cabeça, disserte sobre as funcionalidades desta classe.
+         Lembre-se que ela deve ser genérica para todos os tipos de bloco.
+         
+         */
+    }
+}
