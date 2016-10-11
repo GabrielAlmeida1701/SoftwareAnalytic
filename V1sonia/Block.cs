5046c89e0212840cebc2285e481cf36730814ef8
Amilson  Junior <amilson_junior@outlook.com>
2016-10-11 15:58:31 -0300

    operation class submited

--- V1sonia/Block.cs
+++ V1sonia/Block.cs
@@ -6,12 +6,44 @@ using System.Threading.Tasks;
 
 namespace V1sonia
 {
+    public enum BlockType
+    {
+        INICIO = 0, //main
+        SE = 1,
+        RETORNAR = 2,
+        SE_NAO = 3,
+        PARA = 4 //for(;;){}
+    }
+
     class Block
     {
-        /*
-         Cabeça, disserte sobre as funcionalidades desta classe.
-         Lembre-se que ela deve ser genérica para todos os tipos de bloco.
-         
-         */
+        public BlockType type;
+        public List<Operation> operations;
+        public List<Block> childBlocks;
+
+        public Block(BlockType b_type)
+        {
+            type = b_type;
+        }
+
+        public void AddOperations(Operation op)
+        {
+            operations.Add(op);
+        }
+
+        public void RemoveOperation(Operation op)
+        {
+            operations.Remove(op);
+        }
+
+        public void AddChildBlock(Block n_block)
+        {
+            childBlocks.Add(n_block);
+        }
+
+        public void RemoveChildBlock(Block b_toBeRemoved)
+        {
+            childBlocks.Remove(b_toBeRemoved);
+        }
     }
 }
